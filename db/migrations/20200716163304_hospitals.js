42dc41ffce7740181123cfa93953075a47c0c4e0
Joe Cooper <joecooper2991@gmail.com>
2020-07-16 18:01:55 +0100

    formatted spacing

--- db/migrations/20200716163304_hospitals.js
+++ db/migrations/20200716163304_hospitals.js
@@ -1,14 +1,13 @@
 exports.up = function (knex) {
-    return knex.schema.createTable("hospitals", (hospitalsTable) => {
-      hospitalsTable.increments("hospital_id").primary();
-      hospitalsTable.string("hospital_name");
-      hospitalsTable.timestamp("created_at").defaultTo(knex.fn.now());
-      hospitalsTable.integer("trust_id").references("trusts.trust_id");
-      hospitalsTable.string("status").defaultTo("active");
-    });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTable("hospitals");
-  };
-  
+  return knex.schema.createTable("hospitals", (hospitalsTable) => {
+    hospitalsTable.increments("hospital_id").primary();
+    hospitalsTable.string("hospital_name");
+    hospitalsTable.timestamp("created_at").defaultTo(knex.fn.now());
+    hospitalsTable.integer("trust_id").references("trusts.trust_id");
+    hospitalsTable.string("status").defaultTo("active");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("hospitals");
+};
